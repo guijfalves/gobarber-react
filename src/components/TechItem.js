c84b29a228c22ae80d6d0888359814a39fe3e733
guijfalves <gui.jfalves@gmail.com>
2021-04-14 07:31:13 -0300

    Adicionado componente de linha

---
+++ src/components/TechItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TechItem({ tech, onDelete }) {
+    return (
+        <li>
+            {tech}
+            <button onClick={onDelete} type="button">Remove</button>
+        </li>
+    );
+}
+
+export default TechItem;
